ac29ac36b0b1ba7a98f559b5ce1ab83393cf5470
BBS007 <b.barbesange@outlook.com>
2017-03-10 11:06:18 +0100

    Diverses corrections + relecture partie 1.

--- rapport/model/gloss.sh
+++ rapport/model/gloss.sh
@@ -3,6 +3,6 @@
 echo "\begin{description}"
 for i in `ls glossaire/`; do
 	echo "\item[`head -n 1 glossaire/$i` :] `sed '1d' glossaire/$i`"
-	echo "\vspace{5mm}"
+	echo "\vspace{1mm}"
 done
 echo "\end{description}"
